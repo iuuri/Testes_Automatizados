ee507518ad10336cb3cffc541c45ca2c07f4e132
“Iuri <iuri.souzasantos@gmail.com>
2022-12-04 12:31:28 -0300

    adicionando novos testes e testando variaveis

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -2,6 +2,10 @@
 import { faker } from '@faker-js/faker';
 
 describe('Funcionalidade pré cadastro', () => {
+  let emailFaker = faker.internet.email()
+  let nomeFaker = faker.name.firstName()
+  let sobrenomeFaker = faker.name.lastName()
+
 
   beforeEach(() => {
     cy.visit("http://lojaebac.ebaconline.art.br/my-account/")
@@ -9,13 +13,13 @@ describe('Funcionalidade pré cadastro', () => {
 
   it('Deve completar o pré cadastro com sucesso', () => {
 
-    cy.get('#reg_email').type(faker.internet.email())
+    cy.get('#reg_email').type(emailFaker)
     cy.get('#reg_password').type('!teste@teste!')
     cy.get(':nth-child(4) > .button').click()
 
     cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-    cy.get('#account_first_name').type(faker.name.firstName())
-    cy.get('#account_last_name').type(faker.name.lastName())
+    cy.get('#account_first_name').type(nomeFaker)
+    cy.get('#account_last_name').type(sobrenomeFaker)
     cy.get('.woocommerce-Button').click()
 
     cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
